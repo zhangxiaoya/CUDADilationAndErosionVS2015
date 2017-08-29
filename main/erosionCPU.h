8573696533e9d512ff5d401e9ffc0a4423cfd4be
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-08-29 16:19:19 +0800

    first commit

---
+++ main/erosionCPU.h
@@ -0,0 +1,2 @@
+#pragma once
+void erosionCPU(int* src, int* dst, int width, int height, int radio);
---
+++ main/erosionFuncTemplate.h
@@ -0,0 +1,4 @@
+#pragma once
+void Filter(int* src, int* dst, int* temp, int width, int height, int radio);
+
+void FilterDilation(int* src, int* dst, int* temp, int width, int height, int radio);
